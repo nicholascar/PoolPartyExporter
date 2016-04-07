4b0e2f7ac0a0f6c334c80e83488c3cd953dda065
Nicholas Car <nicholas.car@ga.gov.au>
2016-04-07 18:10:07 +1000

    download function added

--- sparql.py
+++ sparql.py
@@ -6,13 +6,7 @@ from datetime import datetime
 import pytz
 
 
-def project_has_changed(project_id, changed_since_date):
-    URI_TEMPLATE = 'https://{SERVER}/PoolParty/sparql/{PROJECT-ID}'
-    ands_server = 'editor.vocabs.ands.org.au'
-    sparql_uri = URI_TEMPLATE\
-        .replace('{SERVER}', ands_server)\
-        .replace('{PROJECT-ID}', project_id)
-
+def create_authed_session():
     creds = json.load(open('creds.json'))
 
     s = requests.Session()
@@ -22,6 +16,18 @@ def project_has_changed(project_id, changed_since_date):
     }
     s.proxies = proxies
     s.auth = (creds['ands_usr'], creds['ands_pwd'])  # BASIC auth
+
+    return s
+
+
+def project_has_changed(project_id, changed_since_date):
+    URI_TEMPLATE = 'https://{SERVER}/PoolParty/sparql/{PROJECT-ID}'
+    sparql_uri = URI_TEMPLATE\
+        .replace('{SERVER}', json.load(open('creds.json'))['ands_server'])\
+        .replace('{PROJECT-ID}', project_id)
+
+    s = create_authed_session()
+
     params = {
         'query': '''
             PREFIX his: <http://schema.semantic-web.at/ppt/history#>
@@ -47,7 +53,35 @@ def project_has_changed(project_id, changed_since_date):
 
     return changed
 
+
+def pull_vocab(project_id, result_path):
+    URI_TEMPLATE = 'https://{SERVER}/PoolParty/api/projects/{PROJECT-ID}/export'
+    download_uri = URI_TEMPLATE\
+        .replace('{SERVER}', json.load(open('creds.json'))['ands_server'])\
+        .replace('{PROJECT-ID}', project_id)
+
+    s = create_authed_session()
+    s.params = {
+        'format': 'Turtle',
+        'exportModules': 'concepts'
+    }
+    r = s.get(download_uri)
+    if r.status_code == 200:
+        with open(result_path, 'wb') as f:
+            for chunk in r.iter_content(1024):
+                f.write(chunk)
+    else:
+        raise Exception('Didn\'t get an HTTP 200 for the vocab download')
+
+
 project_id = 'GACGIOtherVocabs'
+
 changed_since_date = datetime(2016, 4, 1, 0, 0, 0, 0, pytz.utc)
-print '---'
-print project_has_changed(project_id, changed_since_date)
+print 'Checking ' + project_id + '...'
+if project_has_changed(project_id, changed_since_date):
+    print 'Changes detected'
+    print 'Downloading ' + project_id
+    pull_vocab(project_id, project_id + '.ttl')
+    print 'Complete'
+else:
+    print 'No changes detected'
